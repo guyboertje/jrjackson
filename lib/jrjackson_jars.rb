1fc1677c6b719458169242368ca4165131a7dc81
alex-dr <alex@datarobot.com>
2018-02-08 20:41:03 -0500

    Bump Jackson to v2.9.4 to resolve CVE-2017-15095
    Deletes old versions of jackson from the  repository so they don't get
    included in the final artifact or show up on security scans.
    
    I really am not very comfortable with how this repo checks in binaries;
    I grabbed these from mvnrepository.com, but the author or any other
    contributor could have injected their own binaries with any malicious
    code they want and 99.999% of consumers (read; everyone running
    Logstash) would never know.

--- lib/jrjackson_jars.rb
+++ lib/jrjackson_jars.rb
@@ -2,15 +2,15 @@
 begin
   require 'jar_dependencies'
 rescue LoadError
-  require 'com/fasterxml/jackson/core/jackson-databind/2.9.1/jackson-databind-2.9.1.jar'
-  require 'com/fasterxml/jackson/core/jackson-annotations/2.9.1/jackson-annotations-2.9.1.jar'
-  require 'com/fasterxml/jackson/module/jackson-module-afterburner/2.9.1/jackson-module-afterburner-2.9.1.jar'
-  require 'com/fasterxml/jackson/core/jackson-core/2.9.1/jackson-core-2.9.1.jar'
+  require 'com/fasterxml/jackson/core/jackson-databind/2.9.4/jackson-databind-2.9.4.jar'
+  require 'com/fasterxml/jackson/core/jackson-annotations/2.9.4/jackson-annotations-2.9.4.jar'
+  require 'com/fasterxml/jackson/module/jackson-module-afterburner/2.9.4/jackson-module-afterburner-2.9.4.jar'
+  require 'com/fasterxml/jackson/core/jackson-core/2.9.4/jackson-core-2.9.4.jar'
 end
 
 if defined? Jars
-  require_jar( 'com.fasterxml.jackson.core', 'jackson-databind', '2.9.1' )
-  require_jar( 'com.fasterxml.jackson.core', 'jackson-annotations', '2.9.1' )
-  require_jar( 'com.fasterxml.jackson.module', 'jackson-module-afterburner', '2.9.1' )
-  require_jar( 'com.fasterxml.jackson.core', 'jackson-core', '2.9.1' )
+  require_jar( 'com.fasterxml.jackson.core', 'jackson-databind', '2.9.4' )
+  require_jar( 'com.fasterxml.jackson.core', 'jackson-annotations', '2.9.4' )
+  require_jar( 'com.fasterxml.jackson.module', 'jackson-module-afterburner', '2.9.4' )
+  require_jar( 'com.fasterxml.jackson.core', 'jackson-core', '2.9.4' )
 end
