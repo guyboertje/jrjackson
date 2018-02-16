bdcae1ee03f69d5c37f73e97c7151fd06e4bc7d1
Guy Boertje <guy@elastic.co>
2018-02-16 18:09:52 +0000

    Bump Jackson version to 2.9.4

--- lib/jrjackson/build_info.rb
+++ lib/jrjackson/build_info.rb
@@ -1,23 +1,33 @@
 module JrJackson
   module BuildInfo
     def self.version
-      '0.4.4'
+      '0.4.5'
     end
 
     def self.release_date
-      '2017-10-06'
+      '2018-02-17'
     end
 
     def self.files
-      `git ls-files`.split($/).select{|f| f !~ /\Abenchmarking/}
+      generated_files.concat(git_files)
     end
 
     def self.jackson_version
-      '2.9.1'
+      '2.9.4'
     end
 
     def self.jar_version
-      '1.2.22'
+      '1.2.23'
+    end
+
+    private
+
+    def self.generated_files
+      Dir.glob( %w(pom.xml lib/jrjackson_jars.rb lib/com/fasterxml/jackson/**/*.jar lib/jrjackson/jars/jrjackson-*.jar) )
+    end
+
+    def self.git_files
+      `git ls-files`.split($/).reject{|s| s.start_with?("benchmarking")}
     end
   end
 end
